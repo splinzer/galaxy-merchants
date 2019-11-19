2198294bd785c0f5d6ecd7564b658f86ceb984ef
PETERZHANG <zhanglipeter@gmail.com>
2019-11-19 13:32:01 +0800

    Modified paragrah.py

--- src/paragraph.py
+++ src/paragraph.py
@@ -1,14 +1,46 @@
 #!/usr/bin/env python
-import commands
+# pylint: disable=C0103
+from command import Command
+from question import Question
+
+
 class Paragraph():
     def __init__(self):
-        self.output = []
-        self.cache = {}
-        pass
-    
-    def parse(self, lines):
-        pass
+        self.__cache = {}
+        self.__answer = []
+        self._cmd = Command()
+        self._question = Question()
 
-    def print_output(self):
-        pass
+    @property
+    def cmd(self):
+        return self._cmd
 
+    @cmd.setter
+    def cmd(self, cmd):
+        if not isinstance(cmd, object):
+            raise TypeError
+        self._cmd = cmd
+
+    @property
+    def question(self):
+        return self._question
+
+    @question.setter
+    def question(self, question):
+        if not isinstance(question, object):
+            raise TypeError
+        self._question = question
+
+    def read(self, intput_filename):
+        with open(intput_filename) as f:
+            for line in f:
+                if "?" in line:
+                    pass
+                else:
+                    symbol, value = self.cmd.process_command(line)
+                    self.__cache[symbol] = value
+
+    def output(self):
+        with open('output_file.txt', "w") as f:
+            for i in self.__answer:
+                f.write(i + '\n')
