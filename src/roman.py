ef030c59dbf8ccf08745c88015844072010fef51
PETERZHANG <zhanglipeter@gmail.com>
2019-11-21 13:20:20 +0800

    Add comment for roman.py

--- src/roman.py
+++ src/roman.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# pylint: disable=C0303
 import re
 from types import MappingProxyType
 from exception import InvalidSymbolException, InvalidSyntaxException
@@ -19,13 +20,29 @@ class Roman():
 
     @property
     def valid_symbols(self):
+        """(dict): return valid Roman symbols
+        """
         return self._valid_symbols
 
     @property
     def validator(self):
+        """(str): return the validate syntax for Roman symbol
+        """
         return self._validator
 
-    def roman_to_arabic(self, roman):
+    def roman_to_arabic(self, roman: str) -> int:
+        """The function is to translate roman into arabic numbers
+
+            Args:
+                roman(str): Roman statements
+
+            Returns:
+                total(int): Arabit number
+
+            Raises:
+                InvalidSyntaxException
+
+        """
         total = 0
         previous = 0
         if not self.__validate_roman(roman):
@@ -40,13 +57,35 @@ class Roman():
                 total -= current
         return total
 
-    def get_value_by_symbol(self, symbol):
+    def get_value_by_symbol(self, symbol: str) -> int:
+        """Get the arabic value for it's roman symbol
+
+            Args:
+                symbol(str): Roman symbol
+
+            Returns:
+                (int): corresponding arabic number
+
+            Raises:
+                InvalidSymbolException
+
+        """
         try:
             return self._valid_symbols[symbol]
         except:
             raise InvalidSymbolException(symbol)
 
-    def __validate_roman(self, roman):
+    def __validate_roman(self, roman: str) -> bool:
+        """
+            Validate if roman statement exists syntax error
+
+            Args:
+                symbol(str): Roman symbol
+
+            Returns:
+                (bool): The return value. True for NO syntax error
+
+        """
         if not re.match(self._validator, roman):
             return False
         return True
